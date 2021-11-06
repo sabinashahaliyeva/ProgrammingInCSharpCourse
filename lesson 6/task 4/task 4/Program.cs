72334fca86b078850ad47106916aa5e5f50227b5
sabina <sebine.sahaliyev@gmail.com>
2021-11-06 09:28:18 -0700

    added

---
+++ lesson 6/task 4/task 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ChangePlaceOfNumber(5,11);
+        }
+
+        static void ChangePlaceOfNumber(int a, int b)
+        {
+            a += b;
+            b = a - b;
+            a -= b;
+            Console.WriteLine($"{a},{b} ");
+        }
+    }
+}
